e939f49cee04c259f2d21ec06a3109756fafcf0c
Akanksha1625 <106308663+Akanksha1625@users.noreply.github.com>
2024-02-14 02:10:55 +0530

    Delete FrankBodyBeautyWebsite directory

--- FrankBodyBeautyWebsite/FRANKBODY BEAUTYWEBSITE/src/Components/CartPage/SingleCart/SingleCart.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./singlecart.css"
-
-export const SingleCart = ({image,category})=>{
-    return <div className="cart">
-        <img src={image} alt="category"/>
-        <p>{category}</p>
-    </div>
-}
